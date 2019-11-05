93f47558fe72ea1459a876eaab8f9517efa57f08
Tufan <tufan.vanniekerk@gmail.com>
2019-11-05 19:07:36 +0200

    Added a Test csv to test out the import function

--- src/app/pages/csv-parse/csv-parse.component.ts
+++ src/app/pages/csv-parse/csv-parse.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as Papa from 'papaparse';
 
-
 @Component({
   selector: 'app-csv-parse',
   templateUrl: './csv-parse.component.html',
@@ -12,36 +11,31 @@ export class CsvParseComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    console.log(this.csvGlobal);
   }
 
     csvArray = ""
-    csvGlobal = ""
-  
+    
 
+    
 
   csv2Array(fileInput) {
-    Papa.parse(fileInput.target.files[0], {
+      Papa.parse(fileInput.target.files[0], {
       skipEmptyLines: true,
       header: true,
       complete: function(results){
         this.tableData = results.data;
         this.tableMeta = results.meta;
         this.csvLength = this.tableData["length"];
-        let i: number = 0
+        let i: number = 0;
+        let xmlOutput =""
+        console.log(xmlOutput)
   /** Main loop that outputs XML, with some console.logging to check variables */
   while(i < this.csvLength){
     this.csvArray = this.tableData[i]
-    /**console.log(this.tableMeta);
-    console.log(this.csvLength);
-    console.log(this.tableData[i])
-    console.log(this.csvArray["ipAddress"]);*/
     console.log( `<route>\n\t<user type="E164">${this.csvArray["number"]}</user>\n\t<next type="regex">!(^.*$)!sip:\\1@${this.csvArray["ipAddress"]}!</next>\n</route>\n`);
-    /** 
-    My Attempt in getting a two way bind from inside a while loop.
-    **/
-    this.xmlOutput = `<route>\n\t<user type="E164">${this.csvArray["number"]}</user>\n\t<next type="regex">!(^.*$)!sip:\\1@${this.csvArray["ipAddress"]}!</next>\n</route>\n`
-    this.csvGlobal = this.xmlOutput;
+    let output = (`&ltroute&gt<br>&emsp;&ltuser type="E164"&gt${this.csvArray["number"]}&lt/user&gt<br>&emsp;&ltnext type="regex"&gt!(^.*$)!sip:\\1@${this.csvArray["ipAddress"]}!&lt/next&gt<br>&lt/route&gt<br>`);
+    var myWindow = window.open("", "MsgWindow", "width=200, height=100");
+    myWindow.document.write(output);
     i++
       }
       }
@@ -55,10 +49,6 @@ export class CsvParseComponent implements OnInit {
       let csvArray = this.tableData[i]
       const xmlOutput = `<route>\n\t<user type="E164">${this.csvArray["number"]}</user>\n\t<next type="regex">!(^.*$)!sip:\\1@${this.csvArray["ipAddress"]}!</next>\n</route>\n`
     }*/
+ 
   }
-  printGlobal(){
-    console.log(this.csvGlobal);
-  };
-  
-
-};
+  }
